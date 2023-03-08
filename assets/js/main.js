b8b71991e024f2a8117b3f90f5cf88f88758caeb
Dike-Code <dikeclintonobinna58@gmail.com>
2023-03-08 22:46:17 +0100

    Sprout-time

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,32 @@ const navbar = document.querySelector('.navbar-con');
 const progress = document.querySelector('.progress');
 const percentage = document.querySelector('.progress span');
 const header = document.querySelector('.navigation');
+const stickyLogo = document.querySelector('.nav-logo');
 
 function stickNav() {
    header.classList.toggle('sticky', window.scrollY > 100);
+   if (header.classList.contains('sticky')) {
+      stickyLogo.classList.add('stick');
+   } else {
+      stickyLogo.classList.remove('stick');
+   }
 }
 
 window.addEventListener('scroll', () => {
    stickNav();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+   new Splide('.splide', {
+      type: 'loop',
+      autoplay: true,
+      interval: 6000,
+      arrows: false,
+      pagination: false,
+      wheel: true,
+      pauseOnHover: false,
+      speed: 2000,
+   }).mount();
 });
 
 toggleBar.addEventListener('click', (e) => {
