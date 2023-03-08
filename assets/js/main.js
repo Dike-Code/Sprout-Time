a36c33602f048a959b0e19bb337850395a3716a1
Dike-Code <dikeclintonobinna58@gmail.com>
2023-03-08 15:56:44 +0100

    sprout-time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,33 @@
+/** @format */
 
 const toggleBar = document.querySelector('.toggle');
-const navbar = document.querySelector('.navbar-con')
-const progress = document.querySelector('.progress')
-const percentage = document.querySelector('.progress span')
+const navbar = document.querySelector('.navbar-con');
+const progress = document.querySelector('.progress');
+const percentage = document.querySelector('.progress span');
+const header = document.querySelector('.navigation');
 
-toggleBar.addEventListener('click', (e) => {
-    navbar.classList.toggle('active')
-})
-
-let per = 0;
-function progressLoad(){
-    if(per>=80){
-        progress.style.width = `80%`;
-        percentage.innerHTML = "80%"
-
-    }else{
-        progress.style.width = `${per}%`;
-        percentage.innerHTML = `${per}%`;
-
-    }
-    per++
-
+function stickNav() {
+   header.classList.toggle('sticky', window.scrollY > 100);
 }
 
-setInterval(progressLoad,90)
+window.addEventListener('scroll', () => {
+   stickNav();
+});
+
+toggleBar.addEventListener('click', (e) => {
+   navbar.classList.toggle('active');
+});
+
+let per = 0;
+function progressLoad() {
+   if (per >= 80) {
+      progress.style.width = `80%`;
+      percentage.innerHTML = '80%';
+   } else {
+      progress.style.width = `${per}%`;
+      percentage.innerHTML = `${per}%`;
+   }
+   per++;
+}
+
+setInterval(progressLoad, 90);
